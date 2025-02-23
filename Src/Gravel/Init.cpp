b94c382fbe28f58954f0fb3d6b51a33002ecb51f
Nullify <46113492+NullifyDev@users.noreply.github.com>
2025-02-23 15:31:01 +0000

    Update Init.cpp

--- Src/Gravel/Init.cpp
+++ Src/Gravel/Init.cpp
@@ -2,6 +2,6 @@
 
 namespace Gravel {
     void Init() {
-        Grapple::APP_LOCATION = L"C:\\Program Files\\WindowsApps\\GAMELOFTSA.Asphalt8Airborne_1.0.0.10_x86__0pp20fcewvvtj";
+        Grapple::APP_LOCATION = L"C:\\Program Files\\WindowsApps\\MyApp";
     }
 }
